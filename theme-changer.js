6b78f2b9d1aa65293a4b4b318986e15a686ddcb1
Виктория <98466732+Victoria23022016@users.noreply.github.com>
2023-03-22 19:23:12 +0700

    Delete theme-changer.js

--- theme-changer.js
+++
@@ -1,6 +0,0 @@
-const changer = document.getElementById('theme-changer');
-const html = document.documentElement;
-
-changer.addEventListener('change', function() {
-    html.toggleAttribute('data-theme-dark');
-});
